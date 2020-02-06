38ee191e4cd09e2a015bd64950dec569bccbb3e5
apdonaghy <adonaghy@seattleu.edu>
2020-02-05 18:39:40 -0800

    finished assignment

--- js/main.js
+++ js/main.js
@@ -1,72 +1,51 @@
-/* JS for WATS 3020 Text Adventure */
+
 let choiceList = ['p1'];
 let currentPage = null;
 
-///////////////////////////////////////////////////
-//////// TODOs ///////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-// Fill in the blanks below to complete each TODO task.                       //
-////////////////////////////////////////////////////////////////////////////////
 
-// TODO: Create a function called `getCurrentPage()`. It should accept one
-// parameter, which is the `slug` for the current page. This function will fetch
-// the current page and return a page object using the `slug` value for a key.
+function getCurrentPage(pageNumber){
+     let newPage = storyData[pageNumber];
+     return newPage;
+}
 
+function recordChoice(pageNumber){
+    choiceList.push(pageNumber);
+}
 
-
-// TODO: Create a function called `recordChoice()` that will accept a `slug`
-// parameter and add it to the `choiceList` Array (probably using `push()`).
-
-
-
-// TODO: Create a function called `undoChoice()` that will remove the last
-// `slug` in the `choiceList` Array and then will return the last `slug` in the
-// `choiceList` Array.
+function undoChoice(){
+    choiceList.pop();
+    let slug = choiceList[choiceList.length-1];
+    return slug;
+}
 
 
-
-// TODO: Create two variables: pageContent and choicesUL. Use a DOM selector
-// method (such as querySelector or getElementByID) to set the variable 
-// pageContent to the <p> element with the ID of 'story-text' and set the
-// variable choicesUL to the <ul> element with the ID 'choices'.
-
-// TODO: Create a function called `updatePage()` that accepts a `page` parameter
-// and handles displaying the page in three steps:
-//  1. It should set the text of the pageContent equal to page.text (the text of
-//     the page).
-//  2. For each item in the array page.choices, it should create a new <li>
-//     element with the text of page.choices[i].text. In addition, the <li>
-//     element should have an attribute called 'data-slug' set to
-//     page.choices[i].link.
-//  3. At the end of the function, call the function addEventListeners().
+let pageContent = document.getElementById("story-text");
+let choicesUl = document.querySelector("#choices");
 
 
 
-// TODO: Create a function called `changePage()` that accepts a parameter called
-// `slug` and which handles "turning the page" in three steps:
-//  1. It should call the `recordChoice()` function (and give it the `slug` as
-//     a parameter.
-//  2. It should set the `currentPage` value by calling the `getCurrentPage()`
-//     function (and give it the `slug` as a parameter).
-//  3. It should invoke the `updatePage()` function (and give it the
-//     `currentPage` object as a parameter).
+function updatePage(newPage){
+    pageContent.innerHTML = newPage.text;
+    choicesUl.innerHTML = "";
+    for (let choice of newPage.choices){
+        let newLi = document.createElement('li');
+        newLi.innerHTML = choice.text;
+        newLi.setAttribute('data-slug', choice.link);
+        choicesUl.appendChild(newLi);
+    }
+    addEventListeners();
+}
+
+
+function changePage(slug){
+    recordChoice(slug);
+    let currentPage = getCurrentPage(slug);
+    updatePage(currentPage); 
+}
 
 
 
-///////////////////////////////////////////////////
-//////// Story Data //////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-// Only edit this data to change/enhance the story. Be sure to watch for how  //
-// changes to the story data might affect the mechanisms that output the      //
-// story.                                                                     //
-////////////////////////////////////////////////////////////////////////////////
-// NOTE: The data below is organized as a JS Object. The content for each     //
-// page is stored using a "slug" -- a short alphanumeric identifier (for      //
-// example, "p1", "p2", "homeEnd", etc.). Each page contains a `text`         //
-// property and a `choices` property. The `choices` property is an Array that //
-// contains all of the choices, including the slug that each choice should    //
-// link to.                                                                   //
-////////////////////////////////////////////////////////////////////////////////
+
 
 var storyData = {
     title: "The Crow and the Fox",
@@ -225,13 +204,6 @@ var storyData = {
     }
 };
 
-///////////////////////////////////////////////////
-//////// Main Script /////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-// This script runs the game. You should only edit it if you are attempting a //
-// stretch goal. Otherwise, this script calls the functions that you have     //
-// created above.                                                             //
-////////////////////////////////////////////////////////////////////////////////
 
 let title = document.querySelector('#story-title');
 title.innerHTML = storyData.title;
